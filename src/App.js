9863b88be14c02717ff46426255fd86b94d9cde2
Angelo <angelo24782@gmail.com>
2020-06-27 16:29:30 +0200

    update Card

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Cards, Charts, CountryPicker } from "./components"
 import styles from "./App.module.css";
+import { fetchData } from "./components/api"
 
 function App() {
+
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+
+    const data = fetchData();
+
+    setData(data)
+
+    console.log(data);
+
+  }, [])
+
   return (
     <div className={styles.container}>
-      <Cards />
+      <Cards data={data} />
       <Charts />
       <CountryPicker />
     </div>
