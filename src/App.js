cf8ea8603b1e2568723bdcab30998f04d003d5e9
Angelo <angelo24782@gmail.com>
2020-06-27 15:41:11 +0200

    update componets add file style global

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './App.css';
 import { Cards, Charts, CountryPicker } from "./components"
+import styles from "./App.module.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className={styles.container}>
       <Cards />
       <Charts />
       <CountryPicker />
