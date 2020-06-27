80bb76bd0477c8de195507abea175a921de67fc5
Angelo <angelo24782@gmail.com>
2020-06-27 20:07:03 +0200

    update component Charts

--- src/components/Charts/Charts.js
+++ src/components/Charts/Charts.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { fetchDailyData } from "../api"
+import { Line, Bar } from "react-chartjs-2";
+import styles from "./Charts.module.css"
 
 const Charts = () => {
+
+    const [dailyData, setDailyDate] = useState([])
+
+    useEffect(() => {
+
+        const fetchApi = async () => {
+            setDailyDate(await fetchDailyData())
+        }
+
+        fetchApi();
+
+    });
+
+    const lineChart = (
+        dailyData.length ?
+            <Line
+                data={{
+                    labels: dailyData.map(({ date }) => date),
+                    datasets: [{
+                        data: dailyData.map(({ confirmed }) => confirmed),
+                        label: "Infected",
+                        borderColor: "#3333ff",
+                        fill: true,
+                    }, {
+                        data: dailyData.map(({ deaths }) => deaths),
+                        label: "Deaths",
+                        borderColor: "red",
+                        backgroundColor: "rgba(255, 0, 0, 0.5)",
+                        fill: true,
+                    }],
+                }}
+            /> : null
+    )
+
     return (
-        <div>
-            <h1>Charts</h1>
+        <div className={styles.container}>
+            {lineChart}
         </div>
     )
 }
--- src/components/api/index.js
+++ src/components/api/index.js
@@ -12,3 +12,20 @@ export const fetchData = async () => {
 
     }
 }
+
+export const fetchDailyData = async () => {
+    try {
+        const { data } = await axios.get(`${url}/daily`);
+
+        const modifyData = data.map((dailyData) => ({
+            confirmed: dailyData.confirmed.total,
+            deaths: dailyData.deaths.total,
+            date: dailyData.reportDate,
+        }));
+
+        return modifyData;
+
+    } catch (error) {
+
+    }
+}
